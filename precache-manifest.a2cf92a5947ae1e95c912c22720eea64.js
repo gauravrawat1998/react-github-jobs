dc873f7eec3edaa817af7c5f57ba61434b88fd99
gauravrawat1998 <gauravrawat2025@gmail.com>
2020-09-01 19:21:20 +0530

    Updates

---
+++ precache-manifest.a2cf92a5947ae1e95c912c22720eea64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efe8ac8966153042d3ff2943727d6b9",
+    "url": "/react-github-jobs/index.html"
+  },
+  {
+    "revision": "6e4cc8b22067baf4d152",
+    "url": "/react-github-jobs/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "cfdf5ac3f3aa6307a295",
+    "url": "/react-github-jobs/static/js/2.36851cab.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/react-github-jobs/static/js/2.36851cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4cc8b22067baf4d152",
+    "url": "/react-github-jobs/static/js/main.d1bd585d.chunk.js"
+  },
+  {
+    "revision": "65c878a9071ad5393e98",
+    "url": "/react-github-jobs/static/js/runtime-main.daa8bf77.js"
+  }
+]);
